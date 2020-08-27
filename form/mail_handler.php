f7da322d05f1068d6bd845bba0f719c6f9a3d8b7
Ashking009 <mishrashish0@gmail.com>
2020-08-27 17:34:45 +0530

    images svgs

---
+++ form/mail_handler.php
@@ -0,0 +1,20 @@
+<?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$subject=$_POST['subject'];
+		$msg=$_POST['message'];
+
+		$to='mishrashish0@gmail.com'; 
+		
+		$message="Name :".$name."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", for contacting us!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
