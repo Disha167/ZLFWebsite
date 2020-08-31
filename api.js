9624bc1628204a5a48717146b2cf7df2db1e408c
Ashking009 <mishrashish0@gmail.com>
2020-08-31 16:08:19 +0530

    API and Service

--- api.js
+++ api.js
@@ -1,9 +1,9 @@
 async function get_TRX_ZLF(){
     const response =await fetch('https://nolexserver.com/api/v1/tickers?ticker_id=ZLF_TRX');
     const data = await response.json();
-    const price_ZLF = data.last_price;
-    const input = document.getElementById('TRX_inp').value;
-    document.getElementById('ZLX_rate').textContent = input * price_ZLF;
+    const price_ZLF = data.ask;
+    // const input = document.getElementById('TRX_inp').value;
+    document.getElementById('ZLX_rate').textContent = parseFloat(price_ZLF).toFixed(6);
 };
 
 async function get_ZLF_INR(){
@@ -11,14 +11,11 @@ async function get_ZLF_INR(){
     targetUrl = 'https://api.wazirx.com/api/v2/tickers'
     const response =await fetch(proxyUrl + targetUrl);
     const data = await response.json();
-    const price_INR = data.trxinr.last;
-    const input = document.getElementById('TRX_inp').value;
-    document.getElementById('INR_rate').textContent = input * price_INR;
+    const input = document.getElementById('ZLX_rate').textContent;
+    const price_INR = parseFloat(data.trxinr.open) * parseFloat(input);
+    document.getElementById('INR_rate').textContent = parseFloat(price_INR).toFixed(6);
     };
 
-
-
-function Conversion(){
     get_TRX_ZLF();
     get_ZLF_INR();
-}
+
