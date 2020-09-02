ad999a903ead44c068edea9c98515c256d345309
Ashking009 <mishrashish0@gmail.com>
2020-09-03 00:24:41 +0530

    styled hero

--- js/main.js
+++ js/main.js
@@ -140,6 +140,19 @@
     return false;
   });
 
+  // typing effect in hero
+  if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 50,
+      backSpeed: 10,
+      backDelay: 2000
+    });
+  }
+
   // jQuery counterUp
   $('[data-toggle="counter-up"]').counterUp({
     delay: 10,
