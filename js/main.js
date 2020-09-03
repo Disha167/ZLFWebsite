109ab34c711d5d9a39d6290da16471a7cd32a04f
Ashking009 <mishrashish0@gmail.com>
2020-09-03 18:31:25 +0530

    l lele

--- js/main.js
+++ js/main.js
@@ -4,10 +4,6 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-var path = window.location.pathname;
-var page = path.split("/").pop();
-
-
 !(function($) {
   "use strict";
 
@@ -103,7 +99,7 @@ var page = path.split("/").pop();
       var top = $(this).offset().top,
         bottom = top + $(this).outerHeight();
 
-      if (cur_pos >= top && cur_pos <= bottom && page == 'index.html') {
+      if (cur_pos >= top && cur_pos <= bottom) {
         if (cur_pos <= bottom) {
           main_nav.find('li').removeClass('active');
         }
